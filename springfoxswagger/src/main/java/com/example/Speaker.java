0083b37c82e84f57a57a200e266cde9ff2e0e861
Roman Tsypuk <rtsypuk@lohika.com>
2017-03-26 15:39:53 +0300

    init commit

---
+++ springfoxswagger/src/main/java/com/example/Speaker.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Max;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class Speaker {
+    @NotEmpty
+    @ApiModelProperty(notes = "Model Name upper case without spaces", required = true)
+    String name;
+
+    @Max(value = 100)
+    @ApiModelProperty(notes = "The age of person must be not more than 100", required = true)
+    String age;
+
+    String company;
+
+    public Speaker(String name, String age, String firm) {
+        this.name = name;
+        this.age = age;
+        this.company = firm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
